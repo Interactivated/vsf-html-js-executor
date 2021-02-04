4dffc1787b6599021d08b327533a17eebd957802
Dmitry Schegolikhin <d.shegolihin@gmail.com>
2021-02-04 15:01:08 +0400

    Check on undefined refs.

--- components/vHtmlJsExecutor.js
+++ components/vHtmlJsExecutor.js
@@ -2,11 +2,13 @@ export default {
   name: 'vHtmlJsExecutor',
   methods: {
     vHtmlJsExecutor (refs) {
-      const scripts = refs.getElementsByTagName('script')
-      if (scripts.length > 0) {
-        const jsCode = scripts[0].innerHTML.trim();
-        /* eslint no-eval: 0 */
-        eval(jsCode);
+      if (refs) {
+        const scripts = refs.getElementsByTagName('script')
+        if (scripts.length > 0) {
+          const jsCode = scripts[0].innerHTML.trim();
+          /* eslint no-eval: 0 */
+          eval(jsCode);
+        }
       }
     }
   }
